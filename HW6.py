384932a814795ca7071f71ddba394b5c2b80ce1d
Chase Walters <memotexe@gmail.com>
2024-02-27 22:34:53 -0500

    Final Subtask Of Feature B Complete
    Subtask #4 Complete

--- HW6.py
+++ HW6.py
@@ -3,6 +3,8 @@ PreviousSprintsPoints = 0
 TotalPoints = 0
 totalNumOfSprint = 0
 PList = []
+TotalEHL = 0
+TotalEHH = 0
 print("Welcome to the SCRUM DATA ANALYSIS SYSTEM or SDAS for short.")
 print("Please decide what you wish to do!")
 choice = int(input("Options:\n"
@@ -45,6 +47,20 @@ match choice:
                         AEHH = (item[1] - item[2]) * item[4]
                         item.append(AEHL)
                         item.append(AEHH)
+
+                    for item in PList:
+                        TotalEHL = TotalEHL + item[5]
+                        TotalEHH = TotalEHH + item[6]
+
+                    for item in PList:
+                        print("Name: " + item[0] + "\n"
+                              + "Days Available: " + str(item[1]) + "\n"
+                              + "Days Busy with Scrum Activities: " + str(item[2]) + "\n"
+                              + "Lowest Hours Per Day: " + str(item[3]) + "\n"
+                              + "Highest Hours Per Day: " + str(item[4]) + "\n")
+
+                    print("Teams Total Effort Hours Range: " + str(TotalEHL) + "-" + str(TotalEHH))
+
     case default:
         print("The proper choice wasn't entered, now exiting")
         exit()
